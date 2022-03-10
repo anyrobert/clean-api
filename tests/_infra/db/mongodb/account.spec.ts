24f6f65c0f422a198e10a12c51f69627ed86a221
Robert Gabriel <anyrobert@outlook.com.br>
2022-03-10 16:56:31 -0300

    feat: add MongoHelper

---
+++ tests/_infra/db/mongodb/account.spec.ts
@@ -0,0 +1,11 @@
+import { MongoHelper } from '@/infra/db/mongodb'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+})
