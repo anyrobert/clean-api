4b19539a3252bd2cb82053c31a554cde5e68c33b
Robert Gabriel <anyrobert@outlook.com.br>
2022-03-10 17:14:05 -0300

    test: ensure AccountMongoRepository returns an account on success

--- tests/_infra/db/mongodb/account.spec.ts
+++ tests/_infra/db/mongodb/account.spec.ts
@@ -1,11 +1,29 @@
-import { MongoHelper } from '@/infra/db/mongodb'
+import { AccountMongoRepository, MongoHelper } from '@/infra/db/mongodb'
 
 describe('Account Mongo Repository', () => {
+  let sut: AccountMongoRepository
+
   beforeAll(async () => {
     await MongoHelper.connect()
   })
-
+  beforeEach(() => {
+    sut = new AccountMongoRepository()
+  })
   afterAll(async () => {
     await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
   })
 })
