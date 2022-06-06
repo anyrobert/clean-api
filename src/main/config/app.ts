b2746189b9a1335de23ffac9aa36f1a942ff4f55
Robert Gabriel <anyrobert@outlook.com.br>
2022-06-06 10:32:40 -0300

    feat: ensure signup route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import { useMiddlewares } from './middlewares'
+import { useRoutes } from './routes'
 
 import express from 'express'
 
 const app = express()
 
 useMiddlewares(app)
+useRoutes(app)
 
 export { app }
--- src/main/config/index.ts
+++ src/main/config/index.ts
@@ -1,2 +1,3 @@
 export * from './app'
 export * from './middlewares'
+export * from './routes'
